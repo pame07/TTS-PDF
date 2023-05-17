b22947a1f7eca23b0a4b80429f3ed5631bd42137
Pame <pame.quinteros.h@gmail.com>
2023-05-16 23:20:01 -0400

    fix pauses in narration, .txt better format for reading

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pyttsx3 # Initialize the library
 import PyPDF2
 
 
-pdf_file = open('C:/Users/Seba/Downloads/Howls_of_Zelania_removed.pdf', 'rb')
-read_pdf = PyPDF2.PdfReader(pdf_file, strict=False)
+#pdf_file = open('C:/Users/Seba/Downloads/test.pdf', 'rb')
+book = open('C:/Users/Seba/Downloads/test.txt', 'r', encoding="utf-8").read()\
+        .replace('\n\n', '*newline*')\
+        .replace('\n', ' ')\
+        .replace('*newline*', '\n\n')
+
+#read_pdf = PyPDF2.PdfReader(book, strict=False)
 
 
-number_of_pages = len(read_pdf.pages)
+#number_of_pages = len(read_pdf.pages)
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -15,18 +20,8 @@ engine.setProperty("voice", voices[1].id) # 2 is the 3rd item index
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-50)
 
-for i in range(0, number_of_pages ):
-    # Read the PDF page 
-    page = read_pdf.pages[i]
-    
-    # Extract the text of the PDF page 
-    page_content = page.extract_text()
-        
-    # say method on the engine that passing input text to be spoken 
-    engine.say(page_content) 
-    
-    # run and wait method to processes the voice commands.  
-    engine.runAndWait()
 
+engine.save_to_file(book, "test.mp3")
+# run and wait method to processes the voice commands.  
 engine.runAndWait()
 
