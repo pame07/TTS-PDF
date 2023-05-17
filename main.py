b22bbb0b0d30d8446379d0a57671ee6f5bcd351d
Pame <pame.quinteros.h@gmail.com>
2023-05-17 08:15:20 -0400

    convert from pdf to text

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pyttsx3 # Initialize the library
+import PyPDF2
 
 
+pdf_file = open('C:/Users/Seba/Downloads/test.pdf', 'rb')
 
-#pdf_file = open('C:/Users/Seba/Downloads/test.pdf', 'rb')
-book = open('C:/Users/Seba/Downloads/test.txt', 'r', encoding="utf-8").read()\
+pdfReader = PyPDF2.PdfReader(pdf_file)
+
+print("Pages Number:", len(pdfReader.pages))
+
+file1=open(r"C:/Users/Seba/Downloads/convertedtext.txt","a")
+
+for i in range(0, len(pdfReader.pages)):
+        pageObj = pdfReader.pages[i]
+        text = pageObj.extract_text()
+        file1.writelines(text)
+
+file1.close()
+
+book = open('C:/Users/Seba/Downloads/convertedtext.txt', 'r', encoding='latin-1').read()\
         .replace('\n\n', '*newline*')\
         .replace('\n', ' ')\
         .replace('*newline*', '\n\n')
